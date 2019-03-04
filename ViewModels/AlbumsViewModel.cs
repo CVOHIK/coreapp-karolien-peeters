7fe09721c9022549d53ade3ac77c60b5fed81f70
Karolien Peeters <karopeeters@hotmail.com>
2019-03-04 19:11:27 +0100

    Add project files.

---
+++ ViewModels/AlbumsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModels
+{
+    public class AlbumsViewModel
+    {
+
+        public List<Album> ListAlbums { get; set; }
+        public SelectList Genres { get; set; }
+        public SelectList Artists { get; set; }
+
+        public int artistID { get; set; }
+        public int genreID { get; set; }
+        public string titleFilter { get; set; }
+    }
+}
